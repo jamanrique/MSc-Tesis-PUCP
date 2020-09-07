dd5d21efade4e9c5a02d30b7685f52d775224659
jamanrique <ja.manrique@pm.me>
2020-09-06 23:12:30 -0500

    R simulation

--- 03_thesis/01_document/03_model/03_simulation.jl
+++ 03_thesis/01_document/03_model/03_simulation.jl
@@ -1,4 +1,4 @@
-using Distributions, DataFrames
+using Distributions, DataFrames, Optim, DataFramesMeta
 
 function cdf_Wᵣ(Val::Real,Qₜ::Real,σ :: Real,t::Real)
     if Qₜ <= 0
@@ -8,7 +8,7 @@ function cdf_Wᵣ(Val::Real,Qₜ::Real,σ :: Real,t::Real)
     Cₜ=(-log(1-t))^(1/α)
     β = Qₜ/Cₜ
     W = Weibull(α,β)
-    Wᵣ = cdf(Val,W)
+    Wᵣ = cdf(W,Val)
     return Wᵣ
 end
 
@@ -24,41 +24,65 @@ function rand_Wᵣ(n::Int,Qₜ::Real,σ::Real,t::Real)
     return random
 end
 
-Yʰ = rand_Wᵣ(10000,1,2,0.5)
-Zʰ = ifelse.(Yʰ .< quantile(Yʰ,0.25),1,              ifelse.(Yʰ .< quantile(Yʰ,0.50),2,ifelse.(Yʰ .< quantile(Yʰ,0.75),3,ifelse.(Yʰ .> quantile(Yʰ,0.75),4,0))))
-
-ϕᵢ = ifelse.(Yʰ .< quantile(Yʰ,0.25),0, 
-            ifelse.(Yʰ .< quantile(Yʰ,0.50),quantile(Yʰ,0.25),
-            ifelse.(Yʰ .< quantile(Yʰ,0.75),quantile(Yʰ,0.5),
-            ifelse.(Yʰ .> quantile(Yʰ,0.75),quantile(Yʰ,0.75),0))))
-
-ϕₛ = ifelse.(Yʰ .< quantile(Yʰ,0.25),quantile(Yʰ,0.25),
-             ifelse.(Yʰ .< quantile(Yʰ,0.50),quantile(Yʰ,0.5),
-             ifelse.(Yʰ .< quantile(Yʰ,0.75),quantile(Yʰ,0.75),
-             ifelse.(Yʰ .> quantile(Yʰ,0.75),quantile(Yʰ,0.999999999999),0))))
-
-DF    = DataFrame(ϕᵢ=ϕᵢ,
-        ϕₛ=ϕₛ,
-        X₁ = 1,
-        X₂=rand(Normal(4,2),10000),
-        X₃=rand(Beta(2,4),10000),
-        X₄=rand(Gamma(4,7),10000))
-
-function reg_Wᵣ(DF::DataFrame,ϕᵢ::Int,ϕᵢ₊₁::Int,t::Real)
-
-    function likelihood(Parameters::AbstractVector,ϕᵢ::Int,ϕᵢ₊₁::Int,t::Real)
-        β = Parameters[1]
-        σ = Parameters[2]
+function Lₖ(Parameters::AbstractVector,DF::AbstractDataFrame,ϕᵢ::Int,ϕᵢ₊₁::Int,t::Real)
+    # t=0.5
+    # ϕᵢ = 1
+    # ϕᵢ₊₁ = 2
         Cₜ = -log(1-t)
-        X = DF ## Agregar 1 para col de diseño y quitar limites
-        Qₜ = exp(X*β)
-
-        ## Inicialización de veroslimilitud
-        Sₗₖ = 0
-        ## Identificar la forma matricial de la verosimilitud
-    end
-    
+        X = DF[:,Not([ϕᵢ,ϕᵢ₊₁])]
+       # Controles #
+        if length(Parameters) != ncol(X)+1
+            error("La dimensión de parámetros es errónea. Debe ser igual a la cantidad de columnas de la matriz de diseño más 1")
+        end
+        β = [Parameters[i] for i in 1:ncol(X)]
+        σ = Parameters[ncol(X)+1]
+        Qₜ = exp.(Matrix(X)*β)
+        Lᵢ = DF[:,ϕᵢ]
+        Lₛ = DF[:,ϕᵢ₊₁]
+        Sₗ = 0
+        for i in 1:nrow(X)
+            local Qₜⱼ = Qₜ[i]
+            Sₗ = Sₗ .+ log.(cdf_Wᵣ.(Lₛ[i,:],Qₜⱼ,σ,t)-cdf_Wᵣ.(Lᵢ[i,:],Qₜⱼ,σ,t))
+        end
+        return Sₗ
 end
 
+function Simulation()
+        Yʰ = rand_Wᵣ(10000,1,2,0.5)
+        Zʰ = ifelse.(Yʰ .< quantile(Yʰ,0.25),1, 
+                    ifelse.(Yʰ .< quantile(Yʰ,0.50),2,
+                    ifelse.(Yʰ .< quantile(Yʰ,0.75),3,
+                    ifelse.(Yʰ .> quantile(Yʰ,0.75),4,0))))
 
+        ϕᵢ = ifelse.(Yʰ .< quantile(Yʰ,0.25),0, 
+                    ifelse.(Yʰ .< quantile(Yʰ,0.50),quantile(Yʰ,0.25),
+                    ifelse.(Yʰ .< quantile(Yʰ,0.75),quantile(Yʰ,0.5),
+                    ifelse.(Yʰ .> quantile(Yʰ,0.75),quantile(Yʰ,0.75),0))))
+
+        ϕₛ = ifelse.(Yʰ .< quantile(Yʰ,0.25),quantile(Yʰ,0.25),
+                    ifelse.(Yʰ .< quantile(Yʰ,0.50),quantile(Yʰ,0.5),
+                    ifelse.(Yʰ .< quantile(Yʰ,0.75),quantile(Yʰ,0.75),
+                    ifelse.(Yʰ .> quantile(Yʰ,0.75),quantile(Yʰ,0.999999999999),0))))
+
+        DF    = DataFrame(ϕᵢ=ϕᵢ,
+                ϕₛ=ϕₛ,
+                X₁ = 1,
+                X₂=rand(Normal(4,2),10000),
+                X₃=rand(Beta(2,4),10000),
+                X₄=rand(Gamma(4,7),10000))
+                
+        return DF
 end
+
+DF = Simulation()
+
+function reg_Wᵣ(DF::AbstractDataFrame,ϕᵢ::Int,ϕᵢ₊₁::Int,t::Real)
+    Parameters =[1.0,1.0,1.0,1.0,2.0]
+    lower = [0.01, 0.01, 0.01, 0.01,0]
+
+    func = TwiceDifferentiable(Lₖ(Parameters,DF,1,2,0.5))
+    optimize(vars -> Lₖ(vars,DF,1,2,0.5),lower, Parameters)
+end
+
+reg_Wᵣ(DF,1,2,0.5)
+
